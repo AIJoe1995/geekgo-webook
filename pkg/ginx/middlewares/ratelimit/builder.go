4b71e58f9084ca5f0234d33b69516b0f1ecd5eb9
Oasis <1980131774@qq.com>
2023-10-14 08:56:36 +0800

    修改ginx/ratelimit/builder为注入pkg/ratelimit的版本， 从原来单独的web限流抽出pkg/ratelimit公共限流方法，供web和sms使用

--- pkg/ginx/middlewares/ratelimit/builder.go
+++ pkg/ginx/middlewares/ratelimit/builder.go
@@ -1,32 +1,24 @@
 package ratelimit
 
+// version2
 import (
 	_ "embed"
 	"fmt"
+	"geekgo-webook/pkg/ratelimit"
 	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis/v9"
 	"log"
 	"net/http"
-	"time"
 )
 
 type Builder struct {
-	prefix   string
-	cmd      redis.Cmdable
-	interval time.Duration
-	// 阈值
-	rate int
+	prefix  string
+	limiter ratelimit.Limiter
 }
 
-//go:embed slide_window.lua
-var luaScript string
-
-func NewBuilder(cmd redis.Cmdable, interval time.Duration, rate int) *Builder {
+func NewBuilder(limiter ratelimit.Limiter) *Builder {
 	return &Builder{
-		cmd:      cmd,
-		prefix:   "ip-limiter",
-		interval: interval,
-		rate:     rate,
+		prefix:  "ip-limiter",
+		limiter: limiter,
 	}
 }
 
@@ -56,6 +48,68 @@ func (b *Builder) Build() gin.HandlerFunc {
 
 func (b *Builder) limit(ctx *gin.Context) (bool, error) {
 	key := fmt.Sprintf("%s:%s", b.prefix, ctx.ClientIP())
-	return b.cmd.Eval(ctx, luaScript, []string{key},
-		b.interval.Milliseconds(), b.rate, time.Now().UnixMilli()).Bool()
+	return b.limiter.Limit(ctx, key)
 }
+
+// version1
+//package ratelimit
+//
+//import (
+//	_ "embed"
+//	"fmt"
+//	"github.com/gin-gonic/gin"
+//	"github.com/redis/go-redis/v9"
+//	"log"
+//	"net/http"
+//	"time"
+//)
+//
+//type Builder struct {
+//	prefix   string
+//	cmd      redis.Cmdable
+//	interval time.Duration
+//	// 阈值
+//	rate int
+//}
+//
+////go:embed slide_window.lua
+//var luaScript string
+//
+//func NewBuilder(cmd redis.Cmdable, interval time.Duration, rate int) *Builder {
+//	return &Builder{
+//		cmd:      cmd,
+//		prefix:   "ip-limiter",
+//		interval: interval,
+//		rate:     rate,
+//	}
+//}
+//
+//func (b *Builder) Prefix(prefix string) *Builder {
+//	b.prefix = prefix
+//	return b
+//}
+//
+//func (b *Builder) Build() gin.HandlerFunc {
+//	return func(ctx *gin.Context) {
+//		limited, err := b.limit(ctx)
+//		if err != nil {
+//			log.Println(err)
+//			// 这一步很有意思，就是如果这边出错了
+//			// 要怎么办？
+//			ctx.AbortWithStatus(http.StatusInternalServerError)
+//			return
+//		}
+//		if limited {
+//			log.Println(err)
+//			ctx.AbortWithStatus(http.StatusTooManyRequests)
+//			return
+//		}
+//		ctx.Next()
+//	}
+//}
+//
+//func (b *Builder) limit(ctx *gin.Context) (bool, error) {
+//	key := fmt.Sprintf("%s:%s", b.prefix, ctx.ClientIP())
+//	return b.cmd.Eval(ctx, luaScript, []string{key},
+//		b.interval.Milliseconds(), b.rate, time.Now().UnixMilli()).Bool()
+//}
