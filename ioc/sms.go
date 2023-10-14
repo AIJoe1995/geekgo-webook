eee2728d82b97c8cb09d504da809062ed5e94802
Oasis <1980131774@qq.com>
2023-10-14 08:47:12 +0800

    增加sms的ratelimit和retryable装饰器 实现带有限流和自动重试的短信服务， pkg/ratelimit用lua脚本实现基于redis得滑动窗口限流

--- ioc/sms.go
+++ ioc/sms.go
@@ -3,8 +3,22 @@ package ioc
 import (
 	"geekgo-webook/internal/service/sms"
 	"geekgo-webook/internal/service/sms/memory"
+	"geekgo-webook/internal/service/sms/ratelimit"
+	"geekgo-webook/internal/service/sms/retryable"
+	limiter "geekgo-webook/pkg/ratelimit"
+	"github.com/redis/go-redis/v9"
+	"time"
 )
 
 func InitSMSService() sms.Service {
+	return memory.NewService()
+}
+
+// // 初始化带有限流机制的短信服务
+func InitSMSServiceLimit(cmd redis.Cmdable) sms.Service {
+	// 换内存，还是换别的
+	svc := ratelimit.NewRatelimitSMSService(memory.NewService(),
+		limiter.NewRedisSlidingWindowLimiter(cmd, time.Second, 100))
+	return retryable.NewService(svc, 3)
 	return memory.NewService()
 }
